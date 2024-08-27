752298315c3085d32af931996ccd093f486f36e6
MSI\Phuong Anh <ltpa1203@gmail.com>
2024-08-27 15:25:54 +0700

    commit admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from '../components/Login/login.component';
-import { AdminComponent } from '../components/Admin/admin.component';
-import { DashboardComponent } from '../components/Admin/Dashboard/dashboard.component';
-import { AddProductComponent } from '../components/Admin/Product/add.component';
-import { ListProductComponent } from '../components/Admin/Product/list.component';
+import { LoginComponent } from './components/Login/login.component';
+import { AdminComponent } from './components/Admin/admin.component';
+import { DashboardComponent } from './components/Admin/Dashboard/dashboard.component';
+import { AddProductComponent } from './components/Admin/Product/add.component';
+import { ListProductComponent } from './components/Admin/Product/list.component';
 
 export const routes: Routes = [
     {
